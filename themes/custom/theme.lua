e025513cacf400fe523d470da80af50dee7763a7
Vishwakant Mishra <vkmishra2050@gmail.com>
2020-05-19 08:19:35 +0530

    wallpaper

--- themes/custom/theme.lua
+++ themes/custom/theme.lua
@@ -5,6 +5,7 @@
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
+local wall_image = "~/pix/mikael.png"
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
@@ -97,7 +98,8 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+-- theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = wall_image
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
